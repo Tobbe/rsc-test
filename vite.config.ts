dcd0c1c7d1d64fe2a076751e3945aa719e326262
Tobbe Lundberg <tobbe@tlundberg.com>
2023-10-31 19:18:27 -0400

    rollup plugin for "use client"

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
+import fs from "node:fs";
+
 import { defineConfig } from "vite";
 import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
+
+import type { NormalizedOutputOptions, OutputBundle, OutputChunk } from 'rollup'
+
+function isOutputChunk(chunk: unknown): chunk is OutputChunk {
+  return Array.isArray((chunk as OutputChunk).moduleIds)
+}
+
+const bannerPlugin = () => {
+  return {
+    name: 'banner',
+    async writeBundle (_options: NormalizedOutputOptions, bundle: OutputBundle) {
+      for (const [fileName, bundleInfo] of Object.entries(bundle)) {
+        if (!isOutputChunk(bundleInfo)) {
+          return
+        }
+
+        const code = fs.readFileSync(bundleInfo.moduleIds[0], { encoding: 'utf8' })
+        if (code.startsWith('"use client"') || code.startsWith("'use client'")) {
+          let data = fs.readFileSync(`./dist/${fileName}`, { encoding: 'utf8' })
+          data = `"use client";\n${data}`
+          fs.writeFileSync(`./dist/${fileName}`, data)
+        }
+      }
+    }
+  }
+}
 
 export default defineConfig({
   plugins: [react(), dts({ include: ["lib"] })],
@@ -19,7 +47,10 @@ export default defineConfig({
           react: "React",
           "react/jsx-runtime": "jsxRuntime",
         },
-      }
+      },
+      plugins: [
+        bannerPlugin(),
+      ],
     },
   },
 });
